7b58f1671e9d077d108100205f5cda39373df496
Inbyungjun <2002jun7@gmail.com>
2026-05-19 21:55:03 +0900

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-g++ src/*.cpp -I SDL2/include -L SDL2/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -o game.exe
+g++ src/*.cpp -std=c++17 -pthread -I SDL2/include -L SDL2/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -o game.exe
 
 if %errorlevel% neq 0 (
     echo Build failed
