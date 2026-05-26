82c40ee2d7a786b708c1149bbb4eeedb0999ad7d
azteccamera <100756662+azteccamera@users.noreply.github.com>
2026-05-26 23:46:57 +0900

    Update Sound.h

--- src/Sound.h
+++ src/Sound.h
@@ -8,6 +8,14 @@
 #include "Enemy.h"
 #include "Wall.h"
 
+enum class SoundKind
+{
+    Generic,
+    Footstep,
+    Bottle,
+    Gunshot
+};
+
 struct SoundParticle
 {
     Vec2 pos = { 0.0f, 0.0f };
@@ -17,6 +25,7 @@ struct SoundParticle
     Vec2 source = { 0.0f, 0.0f };
     // 같은 EmitSound()에서 나온 파티클들을 하나의 소음 이벤트로 묶기 위한 값
     int eventId = 0;
+    SoundKind kind = SoundKind::Generic;
     // 소음 이벤트가 생성된 뒤 지난 시간
     float age = 0.0f;
 
@@ -42,6 +51,10 @@ struct HearingResult
     float energy = 0.0f;
     float strongestEnergy = 0.0f;
     Vec2 noisePos = { 0.0f, 0.0f };
+
+    int eventId = 0;
+    SoundKind kind = SoundKind::Generic;
+
     bool heard = false;
 };
 
@@ -51,7 +64,8 @@ void EmitSound(
     int count,
     float speed,
     float loudness = 1.0f,
-    float life = 1.4f);
+    float life = 1.4f,
+    SoundKind kind = SoundKind::Generic);
 
 void PrepareSoundWalls(std::vector<Wall>& walls);
 
